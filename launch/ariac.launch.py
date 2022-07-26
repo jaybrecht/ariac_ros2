9f2d9e8e28630d06d7088059fce9965e8c3ebe00
Justin <jalbrecht23@gmail.com>
2022-07-26 15:00:12 -0400

    simplified basic ariac launch file

---
+++ launch/ariac.launch.py
@@ -0,0 +1,63 @@
+import os
+from launch import LaunchDescription
+from launch.actions import (
+    DeclareLaunchArgument,
+    IncludeLaunchDescription,
+    OpaqueFunction,
+)
+from launch.conditions import IfCondition, UnlessCondition
+from launch.event_handlers import OnProcessExit
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+def launch_setup(context, *args, **kwargs):    
+    # Set the path to this package.
+    pkg_share = FindPackageShare(package='ariac_ros2').find('ariac_ros2')
+    
+    # Set the path to the world file
+    world_file_name = 'ariac.world'
+    world_path = os.path.join(pkg_share, 'worlds', world_file_name)
+    
+    # Set the path to the SDF model files.
+    gazebo_models_path = os.path.join(pkg_share, 'models')
+    os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+
+    # General arguments
+    launch_rviz = LaunchConfiguration("launch_rviz")
+
+    
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=[],
+        condition=IfCondition(launch_rviz),
+    )
+
+    # Gazebo nodes
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [FindPackageShare("gazebo_ros"), "/launch", "/gazebo.launch.py"]
+        ),
+        launch_arguments={'world': world_path}.items()
+    )
+
+    nodes_to_start = [
+        gazebo,
+        rviz_node,
+    ]
+
+    return nodes_to_start
+
+def generate_launch_description():
+    declared_arguments = []
+    
+    declared_arguments.append(
+        DeclareLaunchArgument("launch_rviz", default_value="false", description="Launch RViz?")
+    )
+
+    return LaunchDescription(declared_arguments + [OpaqueFunction(function=launch_setup)])
