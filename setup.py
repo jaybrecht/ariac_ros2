4cc5c87b70146bb927c17ebf39b59099a9965c59
Justin <jalbrecht23@gmail.com>
2022-07-26 14:46:00 -0400

    created gazebo_ariac launch file that can launch a barebones ariac environment

--- setup.py
+++ setup.py
@@ -4,18 +4,42 @@ from setuptools import setup
 
 package_name = 'ariac_ros2'
 
+data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ]
+
+
+def package_files(data_files, directory_list):
+
+    paths_dict = {}
+
+    for directory in directory_list:
+
+        for (path, directories, filenames) in os.walk(directory):
+
+            for filename in filenames:
+
+                file_path = os.path.join(path, filename)
+                install_path = os.path.join('share', package_name, path)
+
+                if install_path in paths_dict.keys():
+                    paths_dict[install_path].append(file_path)
+
+                else:
+                    paths_dict[install_path] = [file_path]
+
+    for key in paths_dict.keys():
+        data_files.append((key, paths_dict[key]))
+
+    return data_files
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=[package_name],
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.urdf')),
-        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
-    ],
+    data_files=package_files(data_files, ['models/', 'launch/', 'worlds/', 'config/', 'urdf']),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='justin',
