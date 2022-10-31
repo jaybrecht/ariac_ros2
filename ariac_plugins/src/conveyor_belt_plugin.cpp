b2c110be71809981429bcc00b42e891f0493683e
Justin Albrecht <justin.albrecht@nist.gov>
2022-10-31 11:44:59 -0400

    added service to control power of conveyor belt

--- ariac_plugins/src/conveyor_belt_plugin.cpp
+++ ariac_plugins/src/conveyor_belt_plugin.cpp
@@ -3,6 +3,7 @@
 #include <ariac_plugins/conveyor_belt_plugin.hpp>
 #include <gazebo_ros/node.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <ariac_msgs/srv/conveyor_belt_control.hpp>
 
 #include <memory>
 
@@ -23,9 +24,18 @@ public:
 
   /// Velocity of the belt joint
   double belt_velocity_;
+  double max_velocity_;
 
   /// Position limit of belt joint to reset 
   double limit_;
+
+  /// Service for enabling the vacuum gripper
+  rclcpp::Service<ariac_msgs::srv::ConveyorBeltControl>::SharedPtr enable_service_;
+
+  /// Callback for enable service
+  void SetConveyorPower(
+    ariac_msgs::srv::ConveyorBeltControl::Request::SharedPtr,
+    ariac_msgs::srv::ConveyorBeltControl::Response::SharedPtr);
 };
 
 ConveyorBeltPlugin::ConveyorBeltPlugin()
@@ -51,10 +61,17 @@ void ConveyorBeltPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr s
   }
 
   // Set velocity (m/s)
-  impl_->belt_velocity_ = 0.2;
+  impl_->max_velocity_ = sdf->GetElement("max_velocity")->Get<double>();
 
   // Set limit (m)
-  impl_->limit_ = 0.4;
+  impl_->limit_ = impl_->belt_joint_->UpperLimit();
+
+    // Register enable service
+  impl_->enable_service_ = impl_->ros_node_->create_service<ariac_msgs::srv::ConveyorBeltControl>(
+      "set_conveyor_power", 
+      std::bind(
+        &ConveyorBeltPluginPrivate::SetConveyorPower, impl_.get(),
+        std::placeholders::_1, std::placeholders::_2));
 
   // Create a connection so the OnUpdate function is called at every simulation iteration. 
   impl_->update_connection_ = gazebo::event::Events::ConnectWorldUpdateBegin(
@@ -69,6 +86,20 @@ void ConveyorBeltPlugin::OnUpdate()
 
   if (belt_position >= impl_->limit_){
     impl_->belt_joint_->SetPosition(0, 0);
+  }
+}
+
+void ConveyorBeltPluginPrivate::SetConveyorPower(
+  ariac_msgs::srv::ConveyorBeltControl::Request::SharedPtr req,
+  ariac_msgs::srv::ConveyorBeltControl::Response::SharedPtr res)
+{
+  res->success = false;
+  if (req->power >= 0 && req->power <= 100) {
+    belt_velocity_ = max_velocity_ * (req->power / 100);
+    res->success = true;
+  }
+  else{
+    RCLCPP_WARN(ros_node_->get_logger(), "Conveyor power must be between 0 and 100");
   }
 }
 
