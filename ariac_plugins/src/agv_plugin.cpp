a6d525bd5fae7c77b0524b01525015b70f139259
Justin Albrecht <justin.albrecht@nist.gov>
2022-11-02 15:10:30 -0400

    added AGV status publisher

--- ariac_plugins/src/agv_plugin.cpp
+++ ariac_plugins/src/agv_plugin.cpp
@@ -6,6 +6,7 @@
 #include <std_msgs/msg/float64_multi_array.hpp>
 #include <sensor_msgs/msg/joint_state.hpp>
 #include <ariac_msgs/srv/move_agv.hpp>
+#include <ariac_msgs/msg/agv_status.hpp>
 
 #include <memory>
 
@@ -35,6 +36,13 @@ public:
   rclcpp::Publisher<std_msgs::msg::Float64MultiArray>::SharedPtr velocity_pub_;
   std_msgs::msg::Float64MultiArray velocity_msg_;
 
+  // AGV Status publisher
+  rclcpp::Publisher<ariac_msgs::msg::AGVStatus>::SharedPtr status_pub_;
+  ariac_msgs::msg::AGVStatus status_msg_;
+
+  rclcpp::Time last_publish_time_;
+  int update_ns_;
+
   // Move service
   rclcpp::Service<ariac_msgs::srv::MoveAGV>::SharedPtr move_service_;
 
@@ -43,6 +51,7 @@ public:
     ariac_msgs::srv::MoveAGV::Request::SharedPtr req,
     ariac_msgs::srv::MoveAGV::Response::SharedPtr res);
   void MoveToGoal(double goal);
+  void PublishStatus();
 
 };
 
@@ -74,6 +83,14 @@ void AGVPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
   std::string vel_topic = "/" + impl_->agv_number_ + "/velocity_controller/commands";
   impl_->velocity_pub_ = impl_->ros_node_->create_publisher<std_msgs::msg::Float64MultiArray>(vel_topic, 10);
 
+  // Register status publisher
+  std::string status_topic = "/ariac/" + impl_->agv_number_ + "_status";
+  impl_->status_pub_ = impl_->ros_node_->create_publisher<ariac_msgs::msg::AGVStatus>(status_topic, 10);
+
+  double publish_rate = 10;
+  impl_->update_ns_ = int((1/publish_rate) * 1e9);
+  impl_->last_publish_time_ = impl_->ros_node_->get_clock()->now();
+
   // Register move service
   impl_->move_service_ = impl_->ros_node_->create_service<ariac_msgs::srv::MoveAGV>(
       "/ariac/move_"+impl_->agv_number_, 
@@ -88,7 +105,12 @@ void AGVPlugin::Load(gazebo::physics::ModelPtr model, sdf::ElementPtr sdf)
 
 void AGVPlugin::OnUpdate()
 {
-  // impl_->PublishVelocity();
+  // Publish status at rate
+  rclcpp::Time now = impl_->ros_node_->get_clock()->now();
+  if (now - impl_->last_publish_time_ >= rclcpp::Duration(0, impl_->update_ns_)) {
+    impl_->PublishStatus();
+    impl_->last_publish_time_ = now;
+  }
 }
 
 
@@ -158,6 +180,24 @@ void AGVPluginPrivate::MoveToGoal(double goal){
 
     this->PublishVelocity(0.0);
   }
+}
+
+void AGVPluginPrivate::PublishStatus(){
+  double error_margin = 0.05; // m
+
+  if (std::abs(agv_joint_->Position(0) - kitting_location_) <= error_margin)
+    status_msg_.location = ariac_msgs::msg::AGVStatus::KITTING;
+  else if (std::abs(agv_joint_->Position(0) - front_assembly_station_) <= error_margin)
+    status_msg_.location = ariac_msgs::msg::AGVStatus::ASSEMBLY_FRONT;
+  else if (std::abs(agv_joint_->Position(0) - back_assembly_station_) <= error_margin)
+    status_msg_.location = ariac_msgs::msg::AGVStatus::ASSEMBLY_BACK;
+  else
+    status_msg_.location = ariac_msgs::msg::AGVStatus::UNKNOWN;
+
+  status_msg_.position = agv_joint_->Position(0);
+  status_msg_.velocity = agv_joint_->GetVelocity(0);
+
+  status_pub_->publish(status_msg_);
 }
 
 // Register this plugin with the simulator
