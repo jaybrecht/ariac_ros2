15199c477028ba7a693900b04fc3c32fd75b22ed
Justin Albrecht <jalbrecht23@gmail.com>
2022-12-28 08:57:08 -0500

    added kitting task scoring to agv tray sensor

--- ariac_sensors/src/agv_tray_sensor_plugin.cpp
+++ ariac_sensors/src/agv_tray_sensor_plugin.cpp
@@ -1,5 +1,7 @@
 #include <ariac_sensors/agv_tray_sensor_plugin.hpp>
+
 #include <gazebo/sensors/LogicalCameraSensor.hh>
+#include <gazebo/physics/physics.hh>
 
 #include <gazebo_ros/conversions/builtin_interfaces.hpp>
 #include <gazebo_ros/conversions/geometry_msgs.hpp>
@@ -9,6 +11,9 @@
 #include <geometry_msgs/msg/pose.hpp>
 
 #include <ariac_msgs/srv/perform_quality_check.hpp>
+#include <ariac_msgs/srv/score_task.hpp>
+
+#include <ariac_msgs/msg/trial.hpp>
 #include <ariac_msgs/msg/quality_issue.hpp>
 #include <ariac_msgs/msg/part.hpp>
 #include <ariac_msgs/msg/part_pose.hpp>
@@ -54,26 +59,36 @@ public:
 
   /// List of orders
   std::vector<ariac_msgs::msg::Order> orders_;
-  std::map<std::string, std::map<int, bool>> faulty_part_info_; 
-  std::map<std::string, std::map<int, std::string>> faulty_part_names_; 
+  std::map<std::string, std::map<int, bool>> order_faulty_info_;
 
   geometry_msgs::msg::Pose sensor_pose_;
 
   rclcpp::Service<ariac_msgs::srv::PerformQualityCheck>::SharedPtr quality_check_service_;
 
+  rclcpp::Service<ariac_msgs::srv::ScoreTask>::SharedPtr score_task_service_;
+
   rclcpp::Publisher<ariac_msgs::msg::Parts>::SharedPtr tray_contents_pub_;
   ariac_msgs::msg::Parts tray_contents_msg_;
+
+  // Trial msg subscriber
+  rclcpp::Subscription<ariac_msgs::msg::Trial>::SharedPtr trial_sub_;
+
+  void TrialConfigCallback(const ariac_msgs::msg::Trial::SharedPtr msg);
 
   /// Publish latest logical camera data to ROS
   void OnUpdate();
 
-  // 
   void PerformQualityCheck(
     ariac_msgs::srv::PerformQualityCheck::Request::SharedPtr,
     ariac_msgs::srv::PerformQualityCheck::Response::SharedPtr
   );
 
-  void FillOrders();
+  void ScoreTask(
+    ariac_msgs::srv::ScoreTask::Request::SharedPtr,
+    ariac_msgs::srv::ScoreTask::Response::SharedPtr
+  );
+
+  ariac_msgs::msg::QualityIssue CheckQuadrantQuality(std::string, ariac_msgs::msg::KittingPart);
 
   TrayParts LocatePartsOnTray();
   
@@ -93,9 +108,8 @@ AGVTraySensorPlugin::~AGVTraySensorPlugin()
 
 void AGVTraySensorPlugin::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementPtr _sdf)
 {
-  this->impl_->sensor_ = std::dynamic_pointer_cast<gazebo::sensors::LogicalCameraSensor>(_sensor);
-  this->impl_->ros_node_ = gazebo_ros::Node::Get(_sdf);
-
+  impl_->sensor_ = std::dynamic_pointer_cast<gazebo::sensors::LogicalCameraSensor>(_sensor);
+  impl_->ros_node_ = gazebo_ros::Node::Get(_sdf);
   
   if (!_sdf->HasElement("sensor_num")) {
     impl_->sensor_num_ = "0";
@@ -103,21 +117,29 @@ void AGVTraySensorPlugin::Load(gazebo::sensors::SensorPtr _sensor, sdf::ElementP
     impl_->sensor_num_ = _sdf->Get<std::string>("sensor_num");
   }
 
-  impl_->FillOrders();
+  impl_->part_types_ = {{"battery", 10}, {"pump", 11}, {"sensor", 12}, {"regulator", 13}}; 
+  impl_->part_colors_ = {{"red", 0}, {"green", 1}, {"blue", 2}, {"orange", 3}, {"purple", 4}};
 
-  this->impl_->part_types_ = {{"battery", 10}, {"pump", 11}, {"sensor", 12}, {"regulator", 13}}; 
-  this->impl_->part_colors_ = {{"red", 0}, {"green", 1}, {"blue", 2}, {"orange", 3}, {"purple", 4}};
-
-  // Create quality control service 
-  impl_->quality_check_service_ = this->impl_->ros_node_->create_service<ariac_msgs::srv::PerformQualityCheck>(
+  // Create services
+  impl_->quality_check_service_ = impl_->ros_node_->create_service<ariac_msgs::srv::PerformQualityCheck>(
   "/ariac/perform_quality_check_agv" + impl_->sensor_num_, 
   std::bind(
     &AGVTraySensorPluginPrivate::PerformQualityCheck, this->impl_.get(),
     std::placeholders::_1, std::placeholders::_2));
 
+  impl_->score_task_service_ = impl_->ros_node_->create_service<ariac_msgs::srv::ScoreTask>(
+  "/ariac/score_task_agv" + impl_->sensor_num_, 
+  std::bind(
+    &AGVTraySensorPluginPrivate::ScoreTask, this->impl_.get(),
+    std::placeholders::_1, std::placeholders::_2));
+
   // Register publisher
   std::string tray_contents_topic = "/ariac/agv" + impl_->sensor_num_ + "_tray_contents";
   impl_->tray_contents_pub_ = impl_->ros_node_->create_publisher<ariac_msgs::msg::Parts>(tray_contents_topic, 10);
+
+  // Subscribe to trial topic
+  impl_->trial_sub_ = impl_->ros_node_->create_subscription<ariac_msgs::msg::Trial>("/ariac/trial_config", 10, 
+    std::bind(&AGVTraySensorPluginPrivate::TrialConfigCallback, this->impl_.get(), std::placeholders::_1));
 
   impl_->sensor_update_event_ = impl_->sensor_->ConnectUpdated(
     std::bind(&AGVTraySensorPluginPrivate::OnUpdate, this->impl_.get()));
@@ -208,53 +230,10 @@ void AGVTraySensorPluginPrivate::PerformQualityCheck(
     return;
   }
 
-  // Determine where each part on tray is located
-  TrayParts parts_on_tray = LocatePartsOnTray();
-
   response->all_passed = true;
   // Check each quadrant with a part for issue
   for (ariac_msgs::msg::KittingPart part : req_order.kitting_task.parts) {    
-    ariac_msgs::msg::QualityIssue issue;
-
-    //  see if a part is present in that quadrant
-    std::map<int, ariac_msgs::msg::PartPose>::iterator it = parts_on_tray.parts.find(part.quadrant);
-    if (it == parts_on_tray.parts.end()){
-      issue.missing_part = true;
-      response->all_passed = false; 
-    }
-
-    // Check to see if the part type is correct
-    if (it->second.part.type != part.part.type) {
-      issue.incorrect_part_type = true;
-      response->all_passed = false;    
-    }
-
-    // Check to see if the part color is correct
-    if (it->second.part.color != part.part.color) {
-      issue.incorrect_part_color = true;
-      response->all_passed = false;  
-    }
-
-    //  check to see if the part is flipped
-    if (CheckFlipped(kit_tray_, it->second)) {
-      issue.flipped_part = true;
-      response->all_passed = false;  
-    }
-
-    // TODO check order to see if the part should be reported faulty
-    if (faulty_part_info_[request->task_id][part.quadrant]) {
-      // Check if first check of faulty part
-      if (faulty_part_names_[request->task_id][part.quadrant] == "")
-      {
-        faulty_part_names_[request->task_id][part.quadrant] = parts_on_tray.part_names[part.quadrant];
-        issue.faulty_part = true;
-        response->all_passed = false;      
-      } else if (faulty_part_names_[request->task_id][part.quadrant] == parts_on_tray.part_names[part.quadrant]) {
-        // Same part is still there
-        issue.faulty_part = true;
-        response->all_passed = false;
-      }
-    }
+    ariac_msgs::msg::QualityIssue issue = CheckQuadrantQuality(request->task_id, part);
 
     if (part.quadrant == ariac_msgs::msg::KittingPart::QUADRANT1)
       response->quadrant1 = issue;
@@ -264,51 +243,85 @@ void AGVTraySensorPluginPrivate::PerformQualityCheck(
       response->quadrant3 = issue;
     else if (part.quadrant == ariac_msgs::msg::KittingPart::QUADRANT4)
       response->quadrant4 = issue;
+    
+    if (!issue.all_passed) {
+      response->all_passed = false;
+    }
   }
 
 }
 
-void AGVTraySensorPluginPrivate::FillOrders()
+void AGVTraySensorPluginPrivate::TrialConfigCallback(const ariac_msgs::msg::Trial::SharedPtr msg)
 {
-  // Create fake order for testing
-  ariac_msgs::msg::Order test_order;
-  test_order.id = "Kjri813s";
-  test_order.type = ariac_msgs::msg::Order::KITTING;
-  test_order.priority = false;
+  orders_ = msg->orders;
 
-  ariac_msgs::msg::KittingTask task;
-  task.tray_id = 3;
-  task.agv_number = 4;
-  task.destination = ariac_msgs::msg::KittingTask::WAREHOUSE;
+  for (auto &challenge : msg->challenges) {
+    if (challenge.type == "faulty_part") {
+      std::map<int, bool> faulty_info;
+      faulty_info[ariac_msgs::msg::KittingPart::QUADRANT1] = challenge.faulty_part_challenge.quandrant1;
+      faulty_info[ariac_msgs::msg::KittingPart::QUADRANT2] = challenge.faulty_part_challenge.quandrant2;
+      faulty_info[ariac_msgs::msg::KittingPart::QUADRANT3] = challenge.faulty_part_challenge.quandrant3;
+      faulty_info[ariac_msgs::msg::KittingPart::QUADRANT4] = challenge.faulty_part_challenge.quandrant4;
 
-  ariac_msgs::msg::KittingPart part1;
-  part1.part.type = ariac_msgs::msg::Part::PUMP;
-  part1.part.color = ariac_msgs::msg::Part::GREEN;
-  part1.quadrant = ariac_msgs::msg::KittingPart::QUADRANT1;
+      order_faulty_info_[challenge.faulty_part_challenge.order_id] = faulty_info;
+      
+    }
+  }
+}
 
-  ariac_msgs::msg::KittingPart part2;
-  part2.part.type = ariac_msgs::msg::Part::SENSOR;
-  part2.part.color = ariac_msgs::msg::Part::GREEN;
-  part2.quadrant = ariac_msgs::msg::KittingPart::QUADRANT3;
+ariac_msgs::msg::QualityIssue AGVTraySensorPluginPrivate::CheckQuadrantQuality(std::string order_id, ariac_msgs::msg::KittingPart part)
+{
+  ariac_msgs::msg::QualityIssue issue;
+  issue.all_passed = true;
 
-  task.parts.push_back(part1);
-  task.parts.push_back(part2);
+  TrayParts parts_on_tray = LocatePartsOnTray();
+  
+  //  see if a part is present in that quadrant
+  std::map<int, ariac_msgs::msg::PartPose>::iterator it = parts_on_tray.parts.find(part.quadrant);
+  if (it == parts_on_tray.parts.end()){
+    issue.missing_part = true;
+    issue.incorrect_part_type = true;
+    issue.incorrect_part_color = true;
+    issue.all_passed = false;
+    return issue;
+  }
 
-  test_order.kitting_task = task;
+  // Check to see if the part type is correct
+  if (it->second.part.type != part.part.type) {
+    issue.incorrect_part_type = true;
+    issue.all_passed = false;    
+  }
 
-  orders_.push_back(test_order);
+  // Check to see if the part color is correct
+  if (it->second.part.color != part.part.color) {
+    issue.incorrect_part_color = true;
+    issue.all_passed = false; 
+  }
 
-  // Enter faulty part info
-  std::map<int, bool> faulty_info;
-  faulty_info.insert({{ariac_msgs::msg::KittingPart::QUADRANT3}, true});
-  faulty_info.insert({{ariac_msgs::msg::KittingPart::QUADRANT1}, false});
+  //  check to see if the part is flipped
+  if (CheckFlipped(kit_tray_, it->second)) {
+    issue.flipped_part = true;
+    issue.all_passed = false;  
+  }
 
-  std::map<int, std::string> faulty_names;
-  faulty_names.insert({{ariac_msgs::msg::KittingPart::QUADRANT3}, ""});
-  faulty_names.insert({{ariac_msgs::msg::KittingPart::QUADRANT1}, ""});
+  // Check order to see if the part should be reported faulty
+  if (order_faulty_info_.find(order_id) != order_faulty_info_.end()) {
+    std::string part_name = parts_on_tray.part_names[part.quadrant];
+    if (order_faulty_info_[order_id][part.quadrant]){
+      issue.faulty_part = true;
+      issue.all_passed = false; 
 
-  faulty_part_info_.insert({test_order.id, faulty_info});
-  faulty_part_names_.insert({test_order.id, faulty_names});
+      // Rename part to faulty and set order faulty info to false
+      gazebo::physics::get_world(sensor_->WorldName())->ModelByName(part_name)->SetName(part_name + "_faulty");
+      order_faulty_info_[order_id][part.quadrant] = false;
+    } else if (part_name.find("faulty") != part_name.npos) {
+      // Part has not been replaced
+      issue.faulty_part = true;
+      issue.all_passed = false; 
+    }
+  }
+
+  return issue;
 
 }
 
@@ -384,6 +397,68 @@ bool AGVTraySensorPluginPrivate::CheckFlipped(
   } else {
     return true;
   }
+}
+
+void AGVTraySensorPluginPrivate::ScoreTask(
+  ariac_msgs::srv::ScoreTask::Request::SharedPtr request,
+  ariac_msgs::srv::ScoreTask::Response::SharedPtr response)
+{
+   // Check if order id is valid
+
+  bool valid_id = false;
+  ariac_msgs::msg::Order req_order;
+  for (const ariac_msgs::msg::Order order: orders_){
+    if (request->order_id == order.id){
+      req_order = order;
+      valid_id = true;
+      break;
+    }    
+  }
+  
+  if (!valid_id) {
+    RCLCPP_INFO(ros_node_->get_logger(), "ID is not valid");
+    return;
+  }
+
+  // Check to see if the correct agv
+  if (req_order.kitting_task.agv_number != std::stoi(sensor_num_)) {
+    RCLCPP_INFO(ros_node_->get_logger(), "Wrong AGV for this order");
+    return;
+  }
+
+  // Check to see that the correct tray is on the AGV
+  if (req_order.kitting_task.tray_id != kit_tray_.id){
+    RCLCPP_INFO(ros_node_->get_logger(), "Tray ID is incorrect");
+    response->score = 0.0;
+    return;
+  }
+
+  // Check each quadrant with a part for issue
+  for (ariac_msgs::msg::KittingPart part : req_order.kitting_task.parts) {    
+    ariac_msgs::msg::QualityIssue issue = CheckQuadrantQuality(request->order_id, part);
+    
+    double quadrant_score = 0;
+
+    if (issue.faulty_part) {
+      continue;
+    }
+
+    if (!issue.incorrect_part_type) {
+      if (!issue.incorrect_part_color) {
+        quadrant_score = 3;
+      } else {
+        quadrant_score = 2; 
+      }
+      
+      if (issue.flipped_part) {
+        quadrant_score -= 1;
+      }
+    }
+
+    response->score += quadrant_score;
+ 
+  }
+  
 }
 
 
