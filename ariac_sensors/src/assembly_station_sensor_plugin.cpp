199254c6aa9a6fc4dfd5f818a2112c9cfdf346b0
Justin Albrecht <jalbrecht23@gmail.com>
2022-12-28 10:05:05 -0500

    added subscription to trial config and service for task scoring

--- ariac_sensors/src/assembly_station_sensor_plugin.cpp
+++ ariac_sensors/src/assembly_station_sensor_plugin.cpp
@@ -7,7 +7,10 @@
 #include <gazebo_ros/node.hpp>
 #include <gazebo_ros/utils.hpp>
 
+#include <ariac_msgs/srv/score_task.hpp>
+
 #include <ariac_msgs/msg/part_pose.hpp>
+#include <ariac_msgs/msg/trial.hpp>
 
 #include <memory>
 
@@ -33,8 +36,25 @@ public:
   std::map<std::string, int> part_types_;
   std::map<std::string, int> part_colors_;
 
+  std::vector<ariac_msgs::msg::Order> orders_;
+
+  std::string sensor_num_;
+
+  // Trial msg subscriber
+  rclcpp::Subscription<ariac_msgs::msg::Trial>::SharedPtr trial_sub_;
+
+  // Task Scoring Service
+  rclcpp::Service<ariac_msgs::srv::ScoreTask>::SharedPtr score_task_service_;
+
   /// Publish latest logical camera data to ROS
   void OnUpdate();
+
+  void TrialConfigCallback(const ariac_msgs::msg::Trial::SharedPtr msg);
+
+  void ScoreTask(
+    ariac_msgs::srv::ScoreTask::Request::SharedPtr,
+    ariac_msgs::srv::ScoreTask::Response::SharedPtr
+  );
 };
 
 AssemblyStationSensorPlugin::AssemblyStationSensorPlugin()
@@ -51,6 +71,12 @@ void AssemblyStationSensorPlugin::Load(gazebo::sensors::SensorPtr _sensor, sdf::
   impl_->sensor_ = std::dynamic_pointer_cast<gazebo::sensors::LogicalCameraSensor>(_sensor);
   impl_->ros_node_ = gazebo_ros::Node::Get(_sdf);
 
+  if (!_sdf->HasElement("sensor_num")) {
+    impl_->sensor_num_ = "0";
+  } else {
+    impl_->sensor_num_ = _sdf->Get<std::string>("sensor_num");
+  }
+
   impl_->part_types_ = {
     {"battery", ariac_msgs::msg::Part::BATTERY},
     {"pump", ariac_msgs::msg::Part::PUMP},
@@ -65,6 +91,17 @@ void AssemblyStationSensorPlugin::Load(gazebo::sensors::SensorPtr _sensor, sdf::
     {"purple", ariac_msgs::msg::Part::PURPLE},
     {"orange", ariac_msgs::msg::Part::ORANGE},
   };
+
+  // Subscribe to trial topic
+  impl_->trial_sub_ = impl_->ros_node_->create_subscription<ariac_msgs::msg::Trial>("/ariac/trial_config", 10, 
+    std::bind(&AssemblyStationSensorPluginPrivate::TrialConfigCallback, this->impl_.get(), std::placeholders::_1));
+
+  // Register Score Task Service
+  impl_->score_task_service_ = impl_->ros_node_->create_service<ariac_msgs::srv::ScoreTask>(
+  "/ariac/score_task_assembly_station" + impl_->sensor_num_, 
+  std::bind(
+    &AssemblyStationSensorPluginPrivate::ScoreTask, this->impl_.get(),
+    std::placeholders::_1, std::placeholders::_2));
 
   impl_->sensor_update_event_ = impl_->sensor_->ConnectUpdated(
     std::bind(&AssemblyStationSensorPluginPrivate::OnUpdate, impl_.get()));
@@ -103,6 +140,37 @@ void AssemblyStationSensorPluginPrivate::OnUpdate()
         break;
       }
     }
+  }
+}
+
+void AssemblyStationSensorPluginPrivate::TrialConfigCallback(const ariac_msgs::msg::Trial::SharedPtr msg)
+{
+  orders_ = msg->orders;
+}
+
+void AssemblyStationSensorPluginPrivate::ScoreTask(
+  ariac_msgs::srv::ScoreTask::Request::SharedPtr request,
+  ariac_msgs::srv::ScoreTask::Response::SharedPtr response)
+{
+  bool valid_id = false;
+  ariac_msgs::msg::Order req_order;
+  for (const ariac_msgs::msg::Order order: orders_){
+    if (request->order_id == order.id){
+      req_order = order;
+      valid_id = true;
+      break;
+    }    
+  }
+  
+  if (!valid_id) {
+    RCLCPP_INFO(ros_node_->get_logger(), "ID is not valid");
+    return;
+  }
+
+  // Check to see if the correct station
+  if (req_order.assembly_task.station != std::stoi(sensor_num_)) {
+    RCLCPP_INFO(ros_node_->get_logger(), "Wrong station for this order");
+    return;
   }
 }
 
