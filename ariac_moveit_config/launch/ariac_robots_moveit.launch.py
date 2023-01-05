1e127b5c5ae4a1a72ac4cd3652d55d9310158541
Justin Albrecht <jalbrecht23@gmail.com>
2023-01-05 11:28:29 -0500

    updated moveit config for the new description file

--- ariac_moveit_config/launch/ariac_robots_moveit.launch.py
+++ ariac_moveit_config/launch/ariac_robots_moveit.launch.py
@@ -70,7 +70,7 @@ def launch_setup(context, *args, **kwargs):
     }
 
     trajectory_execution = {
-        "moveit_manage_controllers": True,
+        "moveit_manage_controllers": False,
         "trajectory_execution.allowed_execution_duration_scaling": 1.2,
         "trajectory_execution.allowed_goal_duration_margin": 0.5,
         "trajectory_execution.allowed_start_tolerance": 0.01,
